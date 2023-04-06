29fb98042461e405c1433251b602f50d16198add
Abderrahman El asri <ael-asri@e2r3p9.1337.ma>
2023-04-06 16:00:13 +0000

    all routes done

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -5,6 +5,7 @@ const verifyToken = (req, res, next) => {
 
 	if (authHeader)
 	{
+		const token = authHeader.split(' ')[1]
 		jwt.verify(token, process.env.JWT_SEC, (err, user) => {
 			if (err)
 				res.status(403).json('Token is not valid!')
@@ -16,4 +17,22 @@ const verifyToken = (req, res, next) => {
 		return res.status(401).json('You are not authenticated!')
 }
 
-module.exports = { verifyToken }
+const verifyTokenAndAuthorization = (req, res, next) => {
+	verifyToken(req, res, () => {
+		if (req.user.id === req.params.id || req.user.isAdmin)
+			next()
+		else
+			res.status(403).json('Not Autorized!')
+	})
+}
+
+const verifyTokenAndAdmin = (req, res, next) => {
+	verifyToken(req, res, () => {
+		if (req.user.isAdmin)
+			next()
+		else
+			res.status(403).json('Not Autorized!')
+	})
+}
+
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin }
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const poductRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
 dotenv.config()
 
@@ -15,6 +18,9 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
+app.use('/api/products', poductRoute)
+app.use('/api/cart', cartRoute)
+app.use('/api/order', orderRoute)
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('listining on 5000..')
